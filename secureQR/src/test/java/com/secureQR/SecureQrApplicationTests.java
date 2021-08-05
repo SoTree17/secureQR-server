d7e6317f9507a37a83f13e8861e5204e69c4cb69
ChoiJunho <ajchoi0928@gmail.com>
2021-08-05 23:25:15 +0900

    REST API Server initial Design

---
+++ secureQR/src/test/java/com/secureQR/SecureQrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.secureQR;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureQrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

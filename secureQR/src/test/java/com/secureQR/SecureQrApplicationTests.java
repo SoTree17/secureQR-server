66cb364038db9530a5bd35a6540f481947ec2251
ChoiJunho <ajchoi0928@gmail.com>
2021-09-02 02:01:05 +0900

    Fix JUnit Test Fail Problem

--- secureQR/src/test/java/com/secureQR/SecureQrApplicationTests.java
+++ secureQR/src/test/java/com/secureQR/SecureQrApplicationTests.java
@@ -2,13 +2,10 @@ package com.secureQR;
 
 import com.secureQR.Domain.DTO.ReqDTO;
 import com.secureQR.Domain.DTO.ResDTO;
-import lombok.extern.slf4j.Slf4j;
 import com.secureQR.Service.SecureQrService_Impl;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ContextConfiguration;
-
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class SecureQrApplicationTests {
@@ -17,35 +14,21 @@ class SecureQrApplicationTests {
 	void contextLoads() {
 	}
 
-	/**
-	 * 비즈니스 로직이 담긴 Service 패키지의 SecureQrServicef를 구현한
-	 * SecureQrService_Impl의 객체 선언이 잘 되는지 테스트한다
 	@Test
-	void test_service_instance(){
+	void test_service_instance() {
 		SecureQrService_Impl sec = new SecureQrService_Impl();
-		assertNotNull(sec);
-		log.info("Test : SecureQrService_Impl instance is valid");
+		Assertions.assertNotNull(sec);
 	}
 
-	*//**
-	 * 데이터를 옮기는데 사용되는 request DTO 인 reqDTO 클래스의 객체가
-	 * 제대로 생성되는지 테스트한다.
-	 *//*
 	@Test
-	void test_reqDTO_instance(){
+	void test_reqDTO_instance() {
 		ReqDTO req = new ReqDTO();
-		assertNotNull(req);
-		log.info("Test : reqDTO instance is valid");
+		Assertions.assertNotNull(req);
 	}
-	*//**
-	 * 응답 데이터를 위해 사용되는 reponse DTO 인 resDTO 클래스의 객체가
-	 * 제대로 생성되는지 테스트한다.
-	 *//*
+
 	@Test
-	void test_resDTO_instance(){
+	void test_resDTO_instance() {
 		ResDTO res = new ResDTO();
-		assertNotNull(res);
-		log.info("Test : resDTO instance is vaild");
+		Assertions.assertNotNull(res);
 	}
-*/
 }
